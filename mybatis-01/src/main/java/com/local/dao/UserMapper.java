286c324a5a7ecce91f191258e1848c834ef5b5ea
huiyuan <huiyuan_zzz@163.com>
2020-02-21 22:15:40 +0800

    Mybatis学习2月21日：万能的Map和模糊查询拓展

--- mybatis-01/src/main/java/com/local/dao/UserMapper.java
+++ mybatis-01/src/main/java/com/local/dao/UserMapper.java
@@ -3,6 +3,7 @@ package com.local.dao;
 import com.local.pojo.User;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author lou ke
@@ -11,14 +12,22 @@ import java.util.List;
  */
 public interface UserMapper {
 
+    //模糊查询
+    List<User> getUserLike(String value);
+
     //查询全部用户
     List<User> getUserList();
 
     //根据id查询用户
     User getUserById(int id);
 
+    User getUserById2(Map<String, Object> map);
+
     //增加一个用户
     int addUser(User user);
+
+    //万能的map
+    int addUser2(Map<String, Object> map);
 
     //修改用户
     int updateUser(User user);
--- mybatis-01/src/test/java/com/local/dao/UserTest.java
+++ mybatis-01/src/test/java/com/local/dao/UserTest.java
@@ -5,7 +5,9 @@ import com.local.utils.MybatisUtils;
 import org.apache.ibatis.session.SqlSession;
 import org.junit.Test;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author lou ke
@@ -13,6 +15,20 @@ import java.util.List;
  * @date 2020/2/21 16:20
  */
 public class UserTest {
+
+    //模糊查询
+    @Test
+    public void testGetUserLike() {
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        List<User> userList = mapper.getUserLike("%李%");
+        for (User user : userList) {
+            System.out.println(user);
+        }
+
+        sqlSession.close();
+    }
+
     @Test
     public void test() {
         //1.获取执行sql操作的sqlSession
@@ -47,6 +63,28 @@ public class UserTest {
         sqlSession.close();
     }
 
+    /*
+     * Map传递参数，直接在sql中取出key即可                     parameterType="map"
+     * 对象传递参数，直接在sql中取出对象的属性即可              parameterType="Object"
+     * 只有一个基本类型参数的情况下，可以直接在sql中取到       省略
+     * 多个参数用 Map,或者注解
+     * */
+    @Test
+    public void testGetUserById2() {
+        //获取sqlSession
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        //获取接口
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        //执行接口的方法
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("id", 3);
+        map.put("name", "王五");
+        System.out.println(mapper.getUserById2(map));
+
+
+        sqlSession.close();
+    }
+
     //增删改需要提交事务
     @Test
     public void testAddUser() {
@@ -58,6 +96,22 @@ public class UserTest {
             System.out.println("插入成功！");
         }
         //提交事务
+        sqlSession.commit();
+
+        sqlSession.close();
+    }
+
+    //字段太多用Map
+    @Test
+    public void testAddUser2() {
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("uid", 5);
+        map.put("name1", "hello");
+        map.put("password1", "1996098");
+        mapper.addUser2(map);
+
         sqlSession.commit();
 
         sqlSession.close();
